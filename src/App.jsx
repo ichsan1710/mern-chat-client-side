151424a14b326e4aafb60bd50e69550497ecf2aa
ichsan1710 <ichsanmochammad@gmail.com>
2023-09-12 22:24:51 +0700

    added api link

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import axios from "axios"
-import Routes from './Routes'
+import axios from "axios";
+import Routes from "./Routes";
 import { UserContextProvider } from "./UserContext.jsx";
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.baseURL = "https://mern-chat-d384dcd3ca9f.herokuapp.com";
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
       <Routes />
     </UserContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
